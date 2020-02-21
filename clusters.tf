eecbe8db771a519230f18e49e44db1f8a103c691
Michiel Bakker <mvbakker@users.noreply.github.com>
2020-02-21 10:19:58 +0100

    Add workflows main.yaml file

--- clusters.tf
+++ clusters.tf
@@ -1,5 +1,5 @@
 module "gke_zero" {
-  source = "github.com/kbst/terraform-kubestack//google/cluster?ref=v0.6.0-beta.0"
+  source = "github.com/kbst/terraform-kubestack//google/cluster?ref=master"
 
   configuration = var.clusters["gke_zero"]
 }
