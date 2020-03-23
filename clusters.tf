eb1899a9d06858e85ccbb0dbd933ceaf7531d095
Michiel Bakker <michiel.bakker@protonmail.com>
2020-03-23 12:59:06 +0100

    Pinned the source to a specific commit

--- clusters.tf
+++ clusters.tf
@@ -1,5 +1,5 @@
 module "gke_zero" {
-  source = "github.com/kbst/terraform-kubestack//google/cluster?ref=master"
+  source = "github.com/kbst/terraform-kubestack//google/cluster?ref=608dcfad3191ba90f180b4b04da640cef751ef9c"
 
   configuration = var.clusters["gke_zero"]
 }
